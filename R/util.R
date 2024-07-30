6f433985ee4156aa1f30a4837c4eb4a285a7df63
phe9480 <99561631+phe9480@users.noreply.github.com>
2024-07-29 23:37:28 -0400

    Add files via upload

--- R/util.R
+++ R/util.R
@@ -9,3 +9,16 @@ lr = function(s){1}
 fh01 = function(s){1-s}
 fh11 = function(s){s*(1-s)}
 
+fw <- function(s, rho, gamma, s.tau){
+  s.til = apply(cbind(s, s.tau), MARGIN=1,FUN=max)
+  w = s.til^rho*(1-s.til)^gamma
+  return(w)
+}
+
+#Note:lr(s) = fw(s, rho=0, gamma=0, s.tau=0)
+#   fh01(s) = fw(s, rho=0, gamma=1, s.tau=0)
+#   fh11(s) = fw(s, rho=1, gamma=1, s.tau=0)
+###################################################
+#To use these internal functions, use nphRshiny:::lr, nphRshiny:::fh01, nphRshiny:::fh11
+#Alternatively, directly load these functions when start the R Shiny app.
+
